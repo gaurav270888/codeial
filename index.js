6dbe98dbc2380ce9ee400286c665bb60a7f14fc8
Gaurav Sharma <spartan270888@gmail.com>
2021-12-04 20:49:11 -0600

    added mongo db connection

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
+// use static variable of express to link our static files like css images and js in assets folder
+app.use(express.static('./assets'));
+
+// use express-ejs-layouts module to design our layouts
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// Route all the requests here to the central router
 app.use('/', require('./routes/index'));
 
 // set up the view engine
@@ -13,7 +24,6 @@ app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the server: ${err}`);
     }
-
     console.log(`Server is running on: ${port}`);
 })
 
