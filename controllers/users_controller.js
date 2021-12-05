8965df5babf3568826df830dad3107e8e8f6da75
Gaurav Sharma <spartan270888@gmail.com>
2021-12-04 23:29:32 -0600

    added sign up, sign in page and implementation of sign up controller/handler/action

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/user');
 
 module.exports.profile = function(req, res) {
     //return res.end('<h1> User Profile </h1>');
@@ -14,4 +14,48 @@ module.exports.posts = function(req, res) {
     return res.render('users', {
         title: 'Users Posts Page'
     });
+}
+
+module.exports.signUp = function(req, res) {
+
+    return res.render('user_sign_up', {
+        title: 'Codeial | Sign Up'
+    });
+}
+
+module.exports.signIn = function(req, res) {
+
+    return res.render('user_sign_in', {
+        title: 'Codeial | Sign In'
+    });
+}
+
+module.exports.create = function(req, res) {
+
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+
+    User.findOne({ email : req.body.email}, function(err, user) {
+        if (err) {
+            console.log('Error in finding Signing Up User');
+            return;
+        }
+
+        if(!user) {
+            User.create(req.body, function(err) {
+                if (err) {
+                    console.log('Error in creating user');
+                }
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession = function(req, res) {
+
+    // TODO later
 }
--- index.js
+++ index.js
@@ -3,19 +3,25 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const cookieParse = require('cookie-parser');
+
+app.use(express.urlencoded());
+
+app.use(cookieParse());
 
 // use static variable of express to link our static files like css images and js in assets folder
 app.use(express.static('./assets'));
 
 // use express-ejs-layouts module to design our layouts
 app.use(expressLayouts);
+// extract Styles and Scripts from the sub pages into the layout and put them in Head and bottom of body respectively 
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 // Route all the requests here to the central router
 app.use('/', require('./routes/index'));
 
-// set up the view engine
+// use ejs module to set up our views
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
