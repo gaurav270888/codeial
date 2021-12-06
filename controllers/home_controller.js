a71a8a045a7d0f2dd2a4db7f97efb4870ecb2561
Gaurav Sharma <spartan270888@gmail.com>
2021-12-06 06:30:40 -0600

    show comments when posts are rendered/loaded

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -14,6 +14,12 @@ module.exports.home = function (req, res) {
     // populate user object for each Post using mongoose populate and exec functions. Look at documentation for more info
     Post.find({}).
         populate('user').
+        populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        }).
         exec(function (err, posts) {
             return res.render('home', {
                 title: 'Codeial | Home',
