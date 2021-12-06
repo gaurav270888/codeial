9b79acd96a93de81350e4ae0e675e44ee452fba5
Gaurav Sharma <spartan270888@gmail.com>
2021-12-06 04:00:28 -0600

    added post functionality for the logged in user

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ router.get('/', homeController.home);
 
 router.use('/users', require('./users'));
 
+router.use('/posts', require('./posts'));
+
 router.use('/advertisment', require('./advertisments'));
 
 module.exports = router;
